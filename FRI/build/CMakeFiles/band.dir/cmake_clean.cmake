c4fa49df2ece4357d67f240de88a9672ca530db7
reza <qrezahosseiny@gmail.com>
2024-01-22 13:58:55 +0330

    convolution

---
+++ FRI/build/CMakeFiles/band.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/band.dir/main.cpp.o"
+  "CMakeFiles/band.dir/main.cpp.o.d"
+  "band"
+  "band.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/band.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
